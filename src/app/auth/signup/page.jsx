21b39fda13dcd7e9cc47772c05600ef385e7ea87
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-25 06:41:16 +0600

    45-4 Setup auth route, HeroUI and Gravity UI icons

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Check} from '@gravity-ui/icons'
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <h2>sign up page</h2>
+            <Check/>
+        </div>
+    );
+};
+
+export default SignUpPage;
